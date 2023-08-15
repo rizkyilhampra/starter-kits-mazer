96025909808af4b718b4217d7d9af5f5e75bc0bc
rizkyilhampra <rizkyilhamp16@gmail.com>
2023-08-16 02:11:08 +0800

    add name and role in navbar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,13 @@
 
 namespace App\Providers;
 
+use App\Models\AuthGroupUser;
+use App\Models\User;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -28,6 +32,18 @@ class AppServiceProvider extends ServiceProvider
 
         Blade::directive('is_invalid', function (string $errorName) {
             return "<?php echo \$errors->has($errorName) ? 'is-invalid' : ''; ?>";
+        });
+
+        View::composer('layouts.layout-vertical.navbar', function ($view) {
+            $view->with('name', auth()->user()->name);
+        });
+
+        View::composer('layouts.layout-vertical.navbar', function ($view) {
+            $role = AuthGroupUser::with(['authGroup' => function ($query) {
+                $query->select('id', 'name');
+            }])->where('user_id', auth()->user()->id)->first('auth_group_id');
+
+            $view->with('role', $role->authGroup->name);
         });
     }
 }
